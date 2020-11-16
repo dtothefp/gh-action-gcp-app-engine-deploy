c2cac1fed3b9d72989f25832ac776c160511a4c1
dtothefp <dtothefp@gmail.com>
2020-11-15 16:23:46 -0800

    add changelog script

---
+++ deploy/updateChangelog.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+const changelogFp = path.resolve(__dirname, '..', 'CHANGELOG.md');
+
+const semverRe = /^##\s\[[0-9]\.[0-9]\.[0-9]\]/
+const { VERSION } = process.env;
+const contents = readFileSync(changelogFp, 'utf8').split('\n');
+const reducer = (re) => {
+  return (idx, str, i) => {
+    if (idx) return idx;
+
+    return re.test(str) ? i : 0;
+  };
+};
+const firstIndex = contents.reduce(reducer(/\[Unreleased\]$/), 0);
+const lastIndex = contents.reduce(reducer(/^\[unreleased\]:/), 0);
+var date = new Date();
+var utcDate = new Date(date.toUTCString());
+
+utcDate.setHours(utcDate.getHours()-8);
+
+var usDate = new Date(utcDate).toLocaleDateString('en-US');
+
+const newContents = [
+  ...contents.slice(0, firstIndex),
+  ...[contents[firstIndex], '\n'],
+  `## [${VERSION}] - ${usDate}`,
+  ...contents.slice(firstIndex + 1, lastIndex),
+  contents[lastIndex].replace(/^(.+?v)[0-9]\.[0-9]\.[0-9](\.\.\.HEAD)$/, (a, b, c) => {
+    return b + VERSION + c;
+  }),
+  contents[lastIndex].replace(/^\[unreleased\](:\s.+?)HEAD$/, (a, b) => {
+    return `[${VERSION}]${b}${VERSION}`;
+  }),
+  ...contents.slice(lastIndex + 1, contents.length)
+];
+
+writeFileSync(changelogFp, newContents.join('\n'), 'utf8');
